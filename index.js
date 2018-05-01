48ca01276e1f60f5e52155fe43c5488a0d4e1c53
Nikki M <nrmonegain@gmail.com>
2018-05-01 17:23:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout (string) { 
+  return string.toUpperCase();
+}
+
+function whisper (string) {
+  return string.toLowerCase();
+}
+
+function logShout (string) {
+  return string.toUpperCase();
+}
+
+function logWhisper(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    console.log("YES INDEED!")
+  } else if  (string === string.toLowerCase()){
+    console.log("I can't hear you!")  
+  } else (string === "I love you, Grandma.") {
+    console.log("I love you, too.")
+  }
+}
